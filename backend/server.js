a8c926bed7cb3344aaea3ac35776df0adc703a3e
Lee Tann <lee.tann@opensesame.com>
2021-07-22 10:27:58 -0700

    add get user route

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import User from './models/userModel.js'
 
 dotenv.config()
 
@@ -12,6 +13,11 @@ app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Hello World')
+})
+app.get('/v1/api/users', async (req, res) => {
+    const allUsers = await User.find()
+
+    res.json(allUsers)
 })
 
 const port = process.env.PORT || 5000
