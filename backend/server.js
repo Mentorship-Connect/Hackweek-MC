6d5976ee18ce6706da327b7658560ec828e9f59e
Lee Tann <lee.tann@opensesame.com>
2021-07-21 17:02:37 -0700

    Base skeleton setup with React and Node and Express

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, console.log(`Server listening at http://localhost:${port}`))
