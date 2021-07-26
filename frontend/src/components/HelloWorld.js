1f4a25976428be7a53a51a4a5d1a62a04f2813ab
Megan <megankob@gmail.com>
2021-07-25 17:50:45 -0700

    learning grids and styling best practices

--- frontend/src/components/HelloWorld.js
+++ frontend/src/components/HelloWorld.js
@@ -6,21 +6,50 @@ import { AuthContext } from '../context/AuthContext'
 import decode from 'jwt-decode'
 import { PhotoCamera } from '@material-ui/icons'
 
+//material ui recommended styles
+import { makeStyles } from '@material-ui/core/styles'
+
+//make a hook to use the material ui makestyles
+const useStyles = makeStyles((theme) => ({
+    //returns an object that contains all the styles for easier user.
+    //provide the styles in here
+    container: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+    }
+}));
+
+
 const HelloWorld = () => {
     console.log('Hello World!');
+    const classes = useStyles();
+    console.log(useStyles());
     return (
        <Fragment>
            <CssBaseline />
-           <main>
+           <main className={classes.container}>
                <div>
-                   <Container maxWidth="sm">
-                        
+                   <Container maxWidth="sm" style={{marginTop: '100px'}}>
                         <Typography varient="h2" align="center" color="textPrimary" gutterBottom> 
                         <PhotoCamera /> Hello World Photo Album
                         </Typography>
                         <Typography varient="h5" align="center" color="textSecondary" paragraph>
                             Hello everyone, this is a photo album and I am practicing using Material UI. Seeing what the paragraph looks like on the screen.
                         </Typography>
+                        <div>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button varient="outlined" color="primary">
+                                        Edit
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button varient="contained" color="primary">
+                                        Add New
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                    </Container>
                </div>
            </main>
