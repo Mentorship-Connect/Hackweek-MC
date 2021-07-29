b7f3cb62087de855d080db7a8e5d65992ac7b578
Megan <megankob@gmail.com>
2021-07-29 09:04:12 -0700

    added muitable test

---
+++ frontend/src/components/MuiTableTest.js
@@ -0,0 +1,61 @@
+import MUIDataTable from "mui-datatables";
+
+const MuiTableTest = (props) => {
+const columns = [
+ {
+  name: "name",
+  label: "Name",
+  options: {
+   filter: true,
+   sort: true,
+  }
+ },
+ {
+  name: "company",
+  label: "Company",
+  options: {
+   filter: true,
+   sort: false,
+  }
+ },
+ {
+  name: "city",
+  label: "City",
+  options: {
+   filter: true,
+   sort: false,
+  }
+ },
+ {
+  name: "state",
+  label: "State",
+  options: {
+   filter: true,
+   sort: false,
+  }
+ },
+];
+
+const data = [
+ { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
+ { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
+ { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
+ { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
+];
+
+const options = {
+  filterType: 'checkbox',
+};
+
+return (
+    <MUIDataTable
+        title={"Employee List"}
+        data={data}
+        columns={columns}
+        options={options}
+    />
+);
+
+}
+
+export default MuiTableTest
