1a20e8d029a560c5898e640443cda6cf1646e133
Megan <megankob@gmail.com>
2021-07-29 11:10:12 -0700

    muitable delete is working

--- frontend/src/components/MuiTableTest.js
+++ frontend/src/components/MuiTableTest.js
@@ -9,7 +9,6 @@ import { Button, Paper, CssBaseline, Typography, Table, TableContainer, TableBod
 import SearchBar from 'material-ui-search-bar'
 import { Edit as EditIcon, Delete as DeleteIcon, } from '@material-ui/icons'
 import useStyles from '../styles'
-
 import MUIDataTable from "mui-datatables";
 
 const MuiTableTest = (props) => {
@@ -30,7 +29,7 @@ const MuiTableTest = (props) => {
     console.log('users list', users)
 
     const handleDelete = async (e, id) => {
-        e.stopPropagation()
+        //e.stopPropagation()
 
         try {
             await deleteUser(id)
@@ -49,56 +48,68 @@ const MuiTableTest = (props) => {
     }
 
 
-const columns = [
- {
-  name: "name",
-  label: "Name",
-  options: {
-   filter: true,
-   sort: true,
-  }
- },
- {
-  name: "email",
-  label: "Email",
-  options: {
-   filter: true,
-   sort: true,
-  }
- },
- {
-  name: "program",
-  label: "Program",
-  options: {
-   filter: true,
-   sort: true,
-  }
- },
- {
-  name: "interests",
-  label: "Interests",
-  options: {
-   filter: true,
-   sort: false,
-  }
- },
+    const columns = [
+    {
+    name: "name",
+    label: "Name",
+    options: {
+    filter: true,
+    sort: true,
+    }
+    },
+    {
+    name: "email",
+    label: "Email",
+    options: {
+    filter: true,
+    sort: true,
+    }
+    },
+    {
+    name: "program",
+    label: "Program",
+    options: {
+    filter: true,
+    sort: true,
+    }
+    },
+    {
+    name: "interests",
+    label: "Interests",
+    options: {
+    filter: true,
+    sort: false,
+    }
+    },
 ];
 
-const options = {
-    filterType: 'checkbox',
-  };
+//onrowsdelete option
 
-return (
-    <Fragment>
-        <MUIDataTable
-            title={"Users"}
-            data={users}
-            columns={columns}
-            options={options}
-        />
-        <Button varient="contained" color="primary" href="/register">Add New</Button>
-    </Fragment>
-);
+    const options = {
+        filterType: 'checkbox',
+        onRowsDelete: (e) => {
+            console.log('On Rows Delete', e.data);
+            for (let i = 0; i < users.length; i++) {
+                console.log('user in onRowsDelete: ', i, e.data[0].index);
+                if (e.data[0].index === i) {
+                    console.log('Yay! We have a match!', users[i]);
+                    handleDelete(e, users[i]._id);
+                }
+            }
+        }
+    };
+
+    return (
+        <Fragment>
+            <MUIDataTable
+                title={"Users"}
+                data={users}
+                columns={columns}
+                options={options}
+            />
+            <Button varient="contained" color="primary" href="/register">Add New</Button>
+        </Fragment>
+    );
 
 }
 
