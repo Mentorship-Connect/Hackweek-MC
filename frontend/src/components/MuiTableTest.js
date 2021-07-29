aca08edec6e519eee7944043b6f3df76a45c60d1
Megan <megankob@gmail.com>
2021-07-29 09:35:13 -0700

    mui table data filtering pagination working

--- frontend/src/components/MuiTableTest.js
+++ frontend/src/components/MuiTableTest.js
@@ -1,6 +1,54 @@
+import React, { useEffect, useState, useContext, Fragment } from 'react'
+import axios from 'axios'
+import { AuthContext } from '../context/AuthContext'
+import { useHistory } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+// Material UI
+import { Button, Paper, CssBaseline, Typography, Table, TableContainer, TableBody, TableCell, TableHead, TableRow, TableSortLabel, TablePagination, FormControlLabel, Switch, IconButton } from '@material-ui/core'
+import SearchBar from 'material-ui-search-bar'
+import { Edit as EditIcon, Delete as DeleteIcon, } from '@material-ui/icons'
+import useStyles from '../styles'
+
 import MUIDataTable from "mui-datatables";
 
 const MuiTableTest = (props) => {
+    const classes = useStyles();
+    let history = useHistory()
+    const { loadUsers, deleteUser, editUser } = useContext(AuthContext)
+    const [users, setUsers] = useState([])
+    console.log('users', users);
+
+
+    useEffect(() => {
+        axios.get('/v1/api/users')
+        .then(res => {
+            setUsers(res.data)
+        }).catch(err => console.log(err))
+    },[loadUsers])
+
+    console.log('users list', users)
+
+    const handleDelete = async (e, id) => {
+        e.stopPropagation()
+
+        try {
+            await deleteUser(id)
+        } catch(error){
+            console.log(error)
+        } 
+    }
+
+    const handleEdit = async (e, id) => {
+        e.stopPropagation()
+        history.push(`users/${id}/update`)
+    }
+
+    const handleUser = (id) => {
+        history.push(`/users/${id}`)
+    }
+
+
 const columns = [
  {
   name: "name",
@@ -11,49 +59,45 @@ const columns = [
   }
  },
  {
-  name: "company",
-  label: "Company",
+  name: "email",
+  label: "Email",
   options: {
    filter: true,
-   sort: false,
+   sort: true,
   }
  },
  {
-  name: "city",
-  label: "City",
+  name: "program",
+  label: "Program",
   options: {
    filter: true,
-   sort: false,
+   sort: true,
   }
  },
  {
-  name: "state",
-  label: "State",
+  name: "interests",
+  label: "Interests",
   options: {
    filter: true,
    sort: false,
   }
  },
-];
-
-const data = [
- { name: "Joe James", company: "Test Corp", city: "Yonkers", state: "NY" },
- { name: "John Walsh", company: "Test Corp", city: "Hartford", state: "CT" },
- { name: "Bob Herm", company: "Test Corp", city: "Tampa", state: "FL" },
- { name: "James Houston", company: "Test Corp", city: "Dallas", state: "TX" },
 ];
 
 const options = {
-  filterType: 'checkbox',
-};
+    filterType: 'checkbox',
+  };
 
 return (
-    <MUIDataTable
-        title={"Employee List"}
-        data={data}
-        columns={columns}
-        options={options}
-    />
+    <Fragment>
+        <MUIDataTable
+            title={"Users"}
+            data={users}
+            columns={columns}
+            options={options}
+        />
+        <Button varient="contained" color="primary" href="/register">Add New</Button>
+    </Fragment>
 );
 
 }
